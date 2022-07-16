ef3fa52fbeb2888f7f03d786d5696da19ec40c72
BayuDC <dev.bayudc@gmail.com>
2022-07-16 22:37:34 +0700

    chore: setup scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    css: ['@/assets/styles/main.scss'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                sass: {
+                    additionalData: [
+                        '@import "@/assets/styles/_variables.scss"',
+                        '@import "@/assets/styles/_mixin.scss"',
+                    ],
+                },
+            },
+        },
+    },
+});
