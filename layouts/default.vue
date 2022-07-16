0e69d24f81cbc036fba5f538157c889945f813d4
BayuDC <dev.bayudc@gmail.com>
2022-07-16 22:39:34 +0700

    chore: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+    title: 'Waifuseum',
+    // titleTemplate: (title) => `My App - ${title}`,
+    viewport: 'width=device-width, initial-scale=1.0',
+    charset: 'utf-8',
+    meta: [{ name: 'description', content: 'The museum of all waifus' }],
+    link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,700&display=swap',
+        },
+    ],
+});
+</script>
+
+<template>
+    <slot />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,3 @@
-<script setup>
-useHead({
-    title: 'Waifuseum',
-    // titleTemplate: (title) => `My App - ${title}`,
-    viewport: 'width=device-width, initial-scale=1.0',
-    charset: 'utf-8',
-    meta: [{ name: 'description', content: 'The museum of all waifus' }],
-    link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,700&display=swap',
-        },
-    ],
-});
-</script>
 <template>
-    <div>
-        <h1>Hello World</h1>
-    </div>
+    <h1>Hello World</h1>
 </template>
