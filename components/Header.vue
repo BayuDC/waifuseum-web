8f84bb11a690f4c0281b2d90f66fd0b8dbd34de6
BayuDC <dev.bayudc@gmail.com>
2022-07-16 22:56:32 +0700

    feat: define the header

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <BaseContainer>
+            <h1>Waifuseum</h1>
+        </BaseContainer>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    background: $black;
+    color: $green;
+
+    .container {
+        height: 80px;
+        display: flex;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 32px;
+        font-style: italic;
+        font-weight: 700;
+    }
+}
+</style>
