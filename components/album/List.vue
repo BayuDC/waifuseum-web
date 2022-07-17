8f0af8ad47c9265aa9a63a76712bdf4ef456e995
BayuDC <dev.bayudc@gmail.com>
2022-07-17 17:57:21 +0700

    feat: link to album page

--- components/album/List.vue
+++ components/album/List.vue
@@ -1,6 +1,14 @@
 <template>
     <ul class="album-list">
         <slot />
+        <li class="more">
+            <NuxtLink to="/albums">
+                <div>
+                    <span>See More</span>
+                    <BaseIcon name="charm:arrow-right" width="30" height="30" />
+                </div>
+            </NuxtLink>
+        </li>
     </ul>
 </template>
 
@@ -9,5 +17,46 @@
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
     gap: 20px;
+
+    .more {
+        a {
+            @include card;
+            display: flex;
+            height: 100%;
+            box-sizing: border-box;
+
+            div {
+                padding: 15px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+
+                width: 100%;
+                border-radius: 10px;
+                font-size: 24px;
+                font-weight: 600;
+                background: rgba($color: $gray, $alpha: 0.16);
+                color: $gray;
+
+                svg {
+                    margin-top: 10px;
+                }
+
+                transition: color 0.3s, background 0.3s, transform 0.3s;
+            }
+            transition: background 0.3s;
+
+            &:hover {
+                background: rgba($color: $green, $alpha: 0.16);
+
+                div {
+                    background: rgba($color: $gray, $alpha: 0);
+                    color: $black;
+                    transform: scale(1.08);
+                }
+            }
+        }
+    }
 }
 </style>
