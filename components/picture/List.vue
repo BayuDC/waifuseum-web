6340f99a1d5711e0eff079dbc941fd483cba7216
BayuDC <dev.bayudc@gmail.com>
2022-07-23 06:52:45 +0700

    feat: load infinite pictures

--- components/picture/List.vue
+++ components/picture/List.vue
@@ -1,12 +1,44 @@
 <script setup>
-defineProps({
+const props = defineProps({
     pictures: Array,
+});
+const emit = defineEmits(['finish']);
+
+const pics = ref(props.pictures);
+const list = ref(null);
+const io = ref(null);
+
+watch(
+    () => props.pictures,
+    () => {
+        pics.value = [...pics.value, ...props.pictures];
+        nextTick(() => {
+            io.value.disconnect();
+            io.value.observe(list.value.lastElementChild);
+        });
+    }
+);
+
+onMounted(() => {
+    io.value = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                emit('finish');
+                io.value.disconnect();
+            }
+        });
+    });
+
+    io.value.observe(list.value.lastElementChild);
+});
+onUnmounted(() => {
+    io.value.disconnect();
 });
 </script>
 
 <template>
-    <ul class="picture-list">
-        <li v-for="picture in pictures" :key="picture.id">
+    <ul class="picture-list" ref="list">
+        <li v-for="picture in pics" :key="picture.id">
             <BasePicture :src="'https://img.waifuseum.my.id/?size=thumbnail&id=' + picture.id" />
         </li>
     </ul>
--- pages/albums/[id].vue
+++ pages/albums/[id].vue
@@ -1,16 +1,30 @@
 <script setup>
 const route = useRoute();
 const id = route.params.id;
+const page = ref(1);
 
 const { data: albumData } = await useFetch(`https://lite.waifuseum.my.id/albums/${id}`, {
     initialCache: false,
 });
-const { data: pictureData } = await useFetch(`https://lite.waifuseum.my.id/albums/${id}/pictures?count=12`, {
-    initialCache: false,
-});
+
+const pageMax = Math.ceil(albumData.value.album.picturesCount / 12);
+
+const { data: pictureData, refresh } = await useFetch(
+    () => `https://lite.waifuseum.my.id/albums/${id}/pictures?count=12&page=${page.value}`,
+    { initialCache: false }
+);
+
+function loadMore() {
+    if (page.value == pageMax) return;
+
+    page.value++;
+    refresh();
+}
 </script>
 
 <template>
-    <AlbumDetail v-bind="albumData.album" />
-    <PictureList :pictures="pictureData.pictures" />
+    <div>
+        <AlbumDetail v-bind="albumData.album" />
+        <PictureList :pictures="pictureData.pictures" @finish="loadMore" />
+    </div>
 </template>
